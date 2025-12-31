62c8a793eff4db660e40f7035063346b7c2ec06f
noah <noahs@unitytechnology.io>
2025-12-31 20:39:38 +1100

    Fix kitty_control test

--- tests/kitty_control.sh
+++ tests/kitty_control.sh
@@ -2,7 +2,7 @@ kitty --listen-on=unix:/tmp/mykitty &
 sleep 1
 kitten @ --to unix:/tmp/mykitty send-text "cd ~/Clone/listpick/src"
 kitten @ --to unix:/tmp/mykitty send-key return
-kitten @ --to unix:/tmp/mykitty send-text "python -m listpick.listpick_app -g ~/Clone/listpick/examples/list_files.toml"
+kitten @ --to unix:/tmp/mykitty send-text "python -m listpick.listpick_app -g ~/Clone/listpick/examples/data_generation/list_files.toml"
 kitten @ --to unix:/tmp/mykitty send-key return
 
 sleep 1
